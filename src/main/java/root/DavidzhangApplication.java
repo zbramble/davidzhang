681f40f3f899d45771a1847117f0e30533d38ad7
David Zhang <zb.ramble@gmail.com>
2020-01-30 00:19:34 -0800

    add init code

---
+++ src/main/java/root/DavidzhangApplication.java
@@ -0,0 +1,13 @@
+package root;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavidzhangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavidzhangApplication.class, args);
+	}
+
+}
