cbae446f42064e8de23131837ab764547941c84a
David Zhang <zb.ramble@gmail.com>
2020-02-10 01:09:57 -0800

    add auto update functionality

--- src/main/java/root/controller/RootController.java
+++ src/main/java/root/controller/RootController.java
@@ -8,7 +8,7 @@ public class RootController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Lalala! Greetings from Spring Boot!";
     }
 
 }
